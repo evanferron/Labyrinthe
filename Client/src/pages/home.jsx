bed59af8658f3c20934436290073dd5905fc14dc
fevan <evan.ferron53@gmail.com>
2023-11-15 11:50:32 +0100

    :sparkles: | create ways, create home and loginregister page

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Main = () => {
+const Home = () => {
     return (
         <div>
-            
+            <p>home</p>
         </div>
     );
 };
 
-export default Main;
+export default Home;
