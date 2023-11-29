cb17d928178e126d034c863bd3de70b233a3a2e0
fevan <evan.ferron53@gmail.com>
2023-11-29 08:41:14 +0100

    form login/register are completed but need to be link to the api

--- Client/src/pages/login.jsx
+++ Client/src/pages/login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        event.target.reset()
+    }
+
     return (
-        <div>
-            <p>login register</p>
+        <div id="main-container-login-register">
+            <a href="/register">Register ➜</a>
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <label>
+                    Email
+                    <input type="text" name="email" className="input-login" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" name="password" className="input-login" />
+                </label>
+                <button>Login</button>
+            </form>
         </div>
     );
 };
--- Client/src/pages/register.jsx
+++ Client/src/pages/register.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
 const Register = () => {
+
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+
+        const nickname = e.target.nickname.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        event.target.reset()
+    }
+    
     return (
-        <div>
-            
-        </div>
+        <div id="main-container-login-register">
+            <a href="/login">Login ➜</a>
+        <form onSubmit={handleSubmit}>
+            <h1>Register</h1>
+            <label>
+                Nickname
+                <input type="text" name="nickname" className="input-login" />
+            </label>
+            <label>
+                Email
+                <input type="text" name="email" className="input-login" />
+            </label>
+            <label>
+                Password
+                <input type="password" name="password" className="input-login" />
+            </label>
+            <button>Register</button>
+        </form>
+
+    </div>
     );
 };
 
